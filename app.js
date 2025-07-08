e852e4ab5d202a1c7fbb5d28316cc631a7096fb3
Thomas <thomandoclases@gmail.com>
2025-07-08 15:55:23 -0300

    Fix: coordenadas ecuatoriales para fecha 9/7/25

--- app.js
+++ app.js
@@ -32,22 +32,17 @@ const LAT_FIJA = -34.56; // latitud de UCU Punta del Este
 const LON_FIJA = -54.56; // longitud de UCU Punta del Este
 
 const coordenadas = [
-  { id: "Aqr", AR: 22.45, Dec: -13.08 },
-  { id: "Ari", AR: 2.37, Dec: 23.55 },
-  { id: "Cnc", AR: 8.69, Dec: 19.51 },
-  { id: "Cap", AR: 21.11, Dec: -17.96 },
-  { id: "Gem", AR: 6.92, Dec: 26.17 },
-  { id: "Leo", AR: 10.66, Dec: 18.95 },
-  { id: "Lib", AR: 15.33, Dec: -16.4 },
-  { id: "Oph", AR: 17.58, Dec: -5.72 },
-  { id: "Psc", AR: 0.76, Dec: 10.35 },
-  { id: "Sgr", AR: 18.96, Dec: -29.36 },
-  { id: "Sco", AR: 16.79, Dec: -31.96 },
-  { id: "Tau", AR: 4.47, Dec: 17.99 },
-  // { id: "Lun", AR: 18.96, Dec: -29.36 },
-  // { id: "Sun", AR: 16.79, Dec: -31.96 },
-  // { id: "Mar", AR: 4.47, Dec: 17.99 },
-
+  { id: "Aqr", AR: 22.27, Dec: -13.04 }, 
+  { id: "Ari", AR: 2.22, Dec: 23.33 }, 
+  { id: "Cnc", AR: 8.41, Dec: 19.30 }, 
+  { id: "Cap", AR: 21.06, Dec: -17.57 }, 
+  { id: "Gem", AR: 6.55, Dec: 26.09 }, 
+  { id: "Leo", AR: 10.39, Dec: 18.57 }, 
+  { id: "Lib", AR: 15.20, Dec: -16.24 },
+  { id: "Psc", AR: 0.45, Dec: 10.21 }, 
+  { id: "Sgr", AR: 18.57, Dec: -29.21 }, 
+  { id: "Sco", AR: 16.47, Dec: -31.57 }, 
+  { id: "Tau", AR: 4.28, Dec: 17.59 }, 
 ];
 
 ///////////////////////////////////
